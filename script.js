b576ad06436885d2043f82d008364dad73f8bb64
Christiane Dumont <30590431+christianedumont@users.noreply.github.com>
2024-01-19 16:49:57 +0100

    Modifs navbar + CSS

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
-const MenuFunctions = {
-    openSidenav: function () {
-        const sidenav = document.querySelector(".sidenav");
-        sidenav.classList.add("active");
-    },
-    closeSidenav: function () {
-        const sidenav = document.querySelector(".sidenav");
-        sidenav.classList.remove("active");
-    },
-    handleScroll: function () {
-        const menuBtn = document.querySelector(".menu-outline");
-        const scrollY = window.scrollY;
+// let menuVisible = false;
+// //Fonction qui cache ou qui  montre le menu
+// function montrerCacherMenu(){
+//     if(menuVisible){
+//         document.getElementById("nav").classList ="";
+//         menuVisible = false;
+//     }else{
+//         document.getElementById("nav").classList ="responsive";
+//         menuVisible = true;
+//     }
+// }
 
-        if (scrollY === 0) {
-            menuBtn.classList.remove("hidden");
-        } else {
-            menuBtn.classList.add("hidden");
-        }
-    },
-};
+// let montreCacheMenu=document.querySelector(".nav-responsive");
+// montreCacheMenu.addEventListener("click",montrerCacherMenu);
 
-document.addEventListener("DOMContentLoaded", function () {
-    const menuBtn = document.querySelector(".menu-outline");
-    const closeBtn = document.querySelector(".close-btn");
-    const sidenavLinks = document.querySelectorAll(".sidenav li a");
 
-    menuBtn.addEventListener("click", MenuFunctions.openSidenav);
-    closeBtn.addEventListener("click", MenuFunctions.closeSidenav);
 
-    sidenavLinks.forEach(link => {
-        link.addEventListener("click", MenuFunctions.closeSidenav);
-    });
+
+
+// function selectionner(){
+//     //cache le menu une fois qu' on  selectionne une option
+//     document.getElementById("nav").classList = "";
+//     menuVisible = false;
+// }
+
+
+// // Sélectionnez tous les liens à l'intérieur de la liste non ordonnée (ul)
+// const liens = document.querySelectorAll("#nav  ul li a");
+
+// // Ajoutez un écouteur d'événement pour le clic à chaque lien
+// liens.forEach(lien => {
+//     lien.addEventListener('click', selectionner );
+// });
+
+
+
+// Sélectionnez les éléments nécessaires
+// Sélectionnez les éléments nécessaires
+const menuIcon = document.querySelector('#navbar-toggle');
+const nav = document.querySelector('#navbar-collapse');
+
+// Ajoutez un écouteur d'événements pour le clic sur l'icône du menu
+menuIcon.addEventListener('click', () => {
+    // Basculez la classe 'active' pour afficher/cacher le menu
+    nav.classList.toggle('active');
+
 });
